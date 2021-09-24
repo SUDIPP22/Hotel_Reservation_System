66482193e5d0e9a8eb5a8b48bc4535c81a675860
SUDIPP22 <panja370@gmail.com>
2021-09-24 21:10:13 +0530

    Added : Method to add hotel details to the list along with test cases

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,5 +1,8 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Purpose : To create Hotel Reservation System
  * @author SUDIP PANJA
@@ -7,17 +10,21 @@ package com.bridgelabz;
  * @since 2021-09-23
  */
 public class HotelReservation {
+    private final List<Hotel> hotels;
+
     public static void main(String[] args) {
         System.out.println("Welcome To Miami Hotel Reservation System");
     }
 
+    public HotelReservation() {
+        this.hotels = new ArrayList<>();
+    }
+
     /**
-     * Purpose : Method to add hotels details with name and rates for regular customer
-     * @param hotelName  takes name of the hotel as first argument
-     * @param weekDayRate takes week day rate of hotels as second argument
-     * @return  the hotel details as name and week day rates.
+     * Purpose : Method to add hotels details to the Hotel List
+     * @return the hotel details
      */
-    public Hotel addHotel(String hotelName, int weekDayRate) {
-        return new Hotel(hotelName, weekDayRate);
+    public boolean addHotel(Hotel hotel) {
+        return this.hotels.add(hotel);
     }
 }
--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -5,21 +5,11 @@ import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
     HotelReservation hotelReservation = new HotelReservation();
-    Hotel hotel1 = hotelReservation.addHotel("Lakewood", 110);
-    Hotel hotel2 = hotelReservation.addHotel("Bridgewood", 160);
-    Hotel hotel3 = hotelReservation.addHotel("Ridgewood", 220);
 
     @Test
-    void givenHotelDetails_WhenAddedHotelName_ShouldReturnEnteredName() {
-        Assertions.assertEquals(hotel1.getHotelName(),"Lakewood");
-        Assertions.assertEquals(hotel2.getHotelName(),"Bridgewood");
-        Assertions.assertEquals(hotel3.getHotelName(),"Ridgewood");
-    }
-
-    @Test
-    void givenHotelDetails_WhenAddedWeekDayRate_ShouldReturnEnteredWeekDayRate() {
-        Assertions.assertEquals(hotel1.getWeekDayRate(),110);
-        Assertions.assertEquals(hotel2.getWeekDayRate(),160);
-        Assertions.assertEquals(hotel3.getWeekDayRate(),220);
+    void givenHotelDetails_WhenAddedHotelDetails_ShouldReturnHotelDetails() {
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("Lakewood", 110)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("Bridgewood", 160)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("Ridgewood", 220)));
     }
 }
